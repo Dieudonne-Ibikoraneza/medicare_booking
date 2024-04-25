6edb16b798eff02578e111f7f7dbdf0dc9301df2
Dieudonne-Ibikoraneza <dieudonneibikoraneza13@gmail.com>
2024-04-25 09:20:01 +0200

    Reuploaded the project setup

---
+++ frontend/src/components/Services/ServiceList.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { services } from '../../assets/data/services.js'
+import ServiceCard from "./ServiceCard";
+
+
+const ServiceList = () => {
+  return <div>ServiceList</div>;
+};
+
+export default ServiceList;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -164,6 +164,19 @@ const Home = () => {
       </section>
 
       <About />
+
+      {/*  Services section */}
+      <section>
+        <div className="container">
+          <div className="xl:w-[470px] mx-auto">
+            <h2 className="heading text-center">Our medical services</h2>
+            <p className="text__para text-center">
+              World-clas care for everyone. Our health System offers unmatched, expert health care.
+            </p>
+          </div>
+        </div>
+</section>
+       {/*  Services section end */}
     </>
   );
 };
