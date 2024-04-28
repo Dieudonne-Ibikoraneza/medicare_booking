4c41db2ce38a3a16b033465ee7e726e51451cb1c
Dieudonne-Ibikoraneza <dieudonneibikoraneza13@gmail.com>
2024-04-28 14:49:06 +0200

    Footer - Halfway there

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,9 +1,80 @@
-import React from 'react'
+/* eslint-disable no-unused-vars */
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../../assets/images/logo.png";
+import { RiLinkedinFill } from "react-icons/ri";
+import {
+  AiFillYoutube,
+  AiFillGithub,
+  AiOutlineInstagram,
+} from "react-icons/ai";
+
+const socialLInks = [
+  {
+    path: "https://github.com/Dieudonne-Ibikoraneza",
+    icon: <AiFillGithub className="group-hover:text-white w-4 h-5" />,
+  },
+  {
+    path: "https://instagram/dieudonne_321",
+    icon: <AiOutlineInstagram className="group-hover:text-white w-4 h-5" />,
+  },
+  {
+    path: "https://youtube.com",
+    icon: <AiFillYoutube className="group-hover:text-white w-4 h-5" />,
+  },
+];
+
+const quickLinks01 = [
+  {
+    path: "/home",
+    display: "Home",
+  },
+  {
+    path: "/",
+    display: "About us",
+  },
+  {
+    path: "/services",
+    display: "Services",
+  },
+  {
+    path: "/",
+    display: "Blog",
+  },
+];
+
+const quickLinks02 = [
+  {
+    path: "/find-a-doctor",
+    display: "Find a Doctor",
+  },
+  {
+    path: "/",
+    display: "Request an Appointment",
+  },
+  {
+    path: "/",
+    display: "Find a Location",
+  },
+  {
+    path: "/",
+    display: "Get a opinion",
+  },
+];
+
+const quickLinks03 = [
+  {
+    path: "/",
+    display: "Donate",
+  },
+  {
+    path: "/contact",
+    display: "Contact Us",
+  },
+];
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+  return <div>Footer</div>;
+};
 
-export default Footer
+export default Footer;
