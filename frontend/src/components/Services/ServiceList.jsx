801fb4690c5e2c3c1da0ee8aba80f7eab4a2c269
Dieudonne-Ibikoraneza <dieudonneibikoraneza13@gmail.com>
2024-04-29 08:14:07 +0200

    Finished the Services page

--- frontend/src/components/Services/ServiceList.jsx
+++ frontend/src/components/Services/ServiceList.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
 import { services } from "../../assets/data/services.js";
 import ServiceCard from "./ServiceCard";
--- frontend/src/pages/Services.jsx
+++ frontend/src/pages/Services.jsx
@@ -1,7 +1,20 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
+import ServiceCard from "../components/Services/ServiceCard";
+import { services } from "../assets/data/services";
 
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <section>
+      <div className="container">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-[30px] mt-[30px]">
+          {services.map((item, index) => (
+            <ServiceCard item={item} index={index} key={index} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Services;
