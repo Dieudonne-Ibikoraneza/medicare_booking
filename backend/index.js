e4b902eca03da98aa33580652dbff1d98bc8052a
Dieudonne-Ibikoraneza <dieudonneibikoraneza13@gmail.com>
2024-04-30 20:45:13 +0200

    Finished mongoDB Database Connection

---
+++ backend/index.js
@@ -0,0 +1,43 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 8080;
+const mongoURL = process.env.MONGO_URL;
+
+const corsOptions = {
+  origin: true,
+};
+
+app.get("/", (req, res) => {
+  res.send("Api is working");
+});
+
+//database connection
+mongoose.set("strictQuery", false);
+const connectDB = async () => {
+  try {
+    await mongoose.connect(mongoURL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB Database is connected");
+  } catch (err) {
+    console.log("Mongodb Database connection failed");
+  }
+};
+//middleware
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors(corsOptions));
+
+app.listen(port, () => {
+  connectDB();
+  console.log("Server is running on port " + port);
+});
