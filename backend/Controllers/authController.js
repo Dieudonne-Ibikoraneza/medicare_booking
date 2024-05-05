376c1502c46ec58674dae17209697e0b78925503
Dieudonne-Ibikoraneza <dieudonneibikoraneza13@gmail.com>
2024-05-05 21:53:54 +0200

    Resolved the register issues

--- backend/Controllers/authController.js
+++ backend/Controllers/authController.js
@@ -1,13 +1,12 @@
 import User from "../models/UserSchema.js";
 import Doctor from "../models/DoctorSchema.js";
-import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
 
 export const register = async (req, res) => {
   const { email, password, name, role, photo, gender } = req.body;
 
   try {
-    let user;
+    let user = null;
 
     if (role === "patient") {
       user = await User.findOne({ email });
@@ -15,11 +14,13 @@ export const register = async (req, res) => {
       user = await Doctor.findOne({ email });
     }
 
+    //check if user exists
+
     if (user) {
       return res.status(400).json({ message: "User already exists" });
     }
 
-    //hash password
+    //hash the password
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
@@ -27,34 +28,29 @@ export const register = async (req, res) => {
       user = new User({
         email,
         password: hashedPassword,
+        name,
         role,
         gender,
         photo,
-        name,
       });
     }
-
     if (role === "doctor") {
       user = new Doctor({
         email,
         password: hashedPassword,
+        name,
         role,
         gender,
         photo,
-        name,
       });
     }
 
-    res
-      .status(200)
-      .json({ success: true, message: "User successfully created!" });
-  } catch (err) {
-    res
-      .status(500)
-      .json({ success: false, message: "Internal server error, Try again!" + err });
-  }
-};
+    await user.save()
 
+    res.status(200).json({ success: true, message: "User created successfully!" });
+  } catch (err) {
+    res.status(500).json({ success: false, message: "Internal server error, Try again!" });}
+};
 export const login = async (req, res) => {
   try {
   } catch (err) {}
