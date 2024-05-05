06b4fe529641d23da851bdcf071113d35c006382
Dieudonne-Ibikoraneza <dieudonneibikoraneza13@gmail.com>
2024-05-05 22:06:37 +0200

    Finished the login auth

--- backend/Controllers/authController.js
+++ backend/Controllers/authController.js
@@ -1,6 +1,17 @@
 import User from "../models/UserSchema.js";
 import Doctor from "../models/DoctorSchema.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+const generateToken = (user) => {
+  return jwt.sign(
+    { id: user._id, role: user.role },
+    process.env.JWT_SECRET_KEY,
+    {
+      expiresIn: "15d",
+    }
+  );
+};
 
 export const register = async (req, res) => {
   const { email, password, name, role, photo, gender } = req.body;
@@ -45,13 +56,66 @@ export const register = async (req, res) => {
       });
     }
 
-    await user.save()
+    await user.save();
 
-    res.status(200).json({ success: true, message: "User created successfully!" });
+    res
+      .status(200)
+      .json({ success: true, message: "User created successfully!" });
   } catch (err) {
-    res.status(500).json({ success: false, message: "Internal server error, Try again!" });}
+    res
+      .status(500)
+      .json({ success: false, message: "Internal server error, Try again!" });
+  }
 };
 export const login = async (req, res) => {
+  const { email } = req.body;
+
   try {
-  } catch (err) {}
+    let user = null;
+
+    const patient = await User.findOne({ email });
+    const doctor = await Doctor.findOne({ email });
+
+    if (patient) {
+      user = patient;
+    }
+    if (doctor) {
+      user = doctor;
+    }
+
+    //check if user exists or not
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    //compare password
+    const isPasswordMatch = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+
+    if (!isPasswordMatch) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid Credentials" });
+    }
+
+    //generate authentication token
+    const token = generateToken(user);
+
+    const { password, role, appointments, ...rest } = user._doc;
+
+    res
+      .status(200)
+      .json({
+        success: true,
+        message: "Successfully login",
+        token,
+        data: { ...rest },
+        role,
+      });
+  } catch (err) {
+    res.status(500).json({ success: false, message: "Failed to login" });
+  }
 };
