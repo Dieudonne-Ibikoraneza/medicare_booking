14a428bf60b727fc4c658764c1889d258ebeb841
Dieudonne-Ibikoraneza <dieudonneibikoraneza13@gmail.com>
2024-05-05 22:19:52 +0200

    Finished the user routes

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import authRoute from './Routes/auth.js'
+import userRoute from './Routes/user.js'
 
 dotenv.config();
 const app = express();
@@ -37,6 +38,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors(corsOptions));
 app.use('/api/v1/auth', authRoute)
+app.use('/api/v1/users', userRoute)
 
 app.listen(port, () => {
   connectDB();
