2508a6be3d6d46e97bc167da99a384aa220e7746
Dieudonne-Ibikoraneza <dieudonneibikoraneza13@gmail.com>
2024-05-07 16:18:30 +0200

    Resolved the get single user controller

--- backend/Controllers/doctorController.js
+++ backend/Controllers/doctorController.js
@@ -53,7 +53,20 @@ export const getSingleDoctor = async (req, res) => {
 
 export const getAllDoctor = async (req, res) => {
   try {
-    const doctors = await Doctor.find({}).select("-password");
+    const { query } = req.query;
+    let doctors;
+
+    if (query) {
+      doctors = Doctor.find({
+        isApproved: "approved",
+        $or: [
+          { name: { $regex: query, $options: "i" } },
+          { name: { $regex: query, $options: "i" } },
+        ],
+      }).sekect("-password");
+    } else {
+      doctors = await Doctor.find({}).select("-password");
+    }
 
     res.status(200).json({
       success: true,
--- backend/Routes/user.js
+++ backend/Routes/user.js
@@ -5,10 +5,11 @@ import {
   getAllUser,
   getSingleUser,
 } from "../Controllers/userController.js";
+import { authenticate } from "./../auth/verifyToken.js";
 
 const router = express.Router();
 
-router.get("/:id", getSingleUser);
+router.get("/:id", authenticate, getSingleUser);
 router.get("/", getAllUser);
 router.put("/:id", updateUser);
 router.delete("/:id", deleteUser);
