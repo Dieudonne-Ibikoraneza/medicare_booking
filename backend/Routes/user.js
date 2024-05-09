b116cb1adc4a6f8140d1b7dd21908387d90ed00a
Dieudonne-Ibikoraneza <dieudonneibikoraneza13@gmail.com>
2024-05-09 11:24:51 +0200

    Finished the authenticate token verification auth

--- backend/Routes/user.js
+++ backend/Routes/user.js
@@ -6,9 +6,11 @@ import {
   getSingleUser,
 } from "../Controllers/userController.js";
 
+import { authenticate } from "../auth/verifyToken.js";
+
 const router = express.Router();
 
-router.get("/:id", getSingleUser);
+router.get("/:id", authenticate, getSingleUser);
 router.get("/", getAllUser);
 router.put("/:id", updateUser);
 router.delete("/:id", deleteUser);
---
+++ backend/auth/verifyToken.js
@@ -0,0 +1,31 @@
+import jwt from "jsonwebtoken";
+import User from "../models/UserSchema.js";
+import Doctor from "../models/DoctorSchema.js";
+
+export const authenticate = async (req, res, next) => {
+  const authToken = req.headers.authorization;
+
+  if (!authToken || !authToken.startsWith("Bearer ")) {
+    return res
+      .status(401)
+      .json({ success: false, message: "No token, authorization denied" });
+  }
+
+  try {
+    const token = authToken.split(" ")[1];
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+
+    req.userId = decoded.id;
+    req.role = decoded.role;
+
+    next();
+  } catch (err) {
+    if ((err.name = "TokenExpiredError")) {
+      return res.status(401).json({ message: "Token is Expired" });
+    }
+      
+      return res.status(401).json({ success: false, mesage: "Invalid token" });
+  }
+};
+
