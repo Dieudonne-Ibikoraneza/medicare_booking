0c0ab38dbe97733f7dbfe8cb83ddf6d46ccb1cd0
Dieudonne-Ibikoraneza <dieudonneibikoraneza13@gmail.com>
2024-05-16 22:05:06 +0200

    Initial Commit on doctor's dashboard

--- frontend/src/Dashboard/doctor-account/Dashboard.jsx
+++ frontend/src/Dashboard/doctor-account/Dashboard.jsx
@@ -1,5 +1,26 @@
+import Loader from "../../components/loader/Loading"
+import Error from "../../components/Error/Error"
+import useGetProfile from "../../hooks/useFetchData"
+import { BASE_URL } from "../../config";
+import Tabs from "./Tabs";
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  
+  const { data, loading, error } = useGetProfile(`${BASE_URL}/doctors/profile/me`)
+  
+  return <section>
+    <div className="max-w-[1170px] px-5 mx-auto">
+      {loading && !error && <Loader />}
+      {error && !loading && <Error />}
+
+      {
+
+        !loading && !error && <div className="grid lg:grid-cols-3 gap-[30px] lg:gap-[50px]">
+          <Tabs />
+        </div>
+      }
+    </div>
+  </section>;
 };
 
 export default Dashboard;
