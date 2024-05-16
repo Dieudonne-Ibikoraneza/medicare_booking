3fe9aedd9ecb330cc4181450595f67760eafcade
Dieudonne-Ibikoraneza <dieudonneibikoraneza13@gmail.com>
2024-05-16 23:27:50 +0200

    Finished the dashboard title section

--- frontend/src/Dashboard/doctor-account/Dashboard.jsx
+++ frontend/src/Dashboard/doctor-account/Dashboard.jsx
@@ -1,26 +1,33 @@
-import Loader from "../../components/loader/Loading"
-import Error from "../../components/Error/Error"
-import useGetProfile from "../../hooks/useFetchData"
+import Loader from "../../components/loader/Loading";
+import Error from "../../components/Error/Error";
+import useGetProfile from "../../hooks/useFetchData";
 import { BASE_URL } from "../../config";
 import Tabs from "./Tabs";
+import { useState } from "react";
 
 const Dashboard = () => {
-  
-  const { data, loading, error } = useGetProfile(`${BASE_URL}/doctors/profile/me`)
-  
-  return <section>
-    <div className="max-w-[1170px] px-5 mx-auto">
-      {loading && !error && <Loader />}
-      {error && !loading && <Error />}
+  const { data, loading, error } = useGetProfile(
+    `${BASE_URL}/doctors/profile/me`
+  );
 
-      {
+  const [tab, setTab] = useState("overview")
 
-        !loading && !error && <div className="grid lg:grid-cols-3 gap-[30px] lg:gap-[50px]">
-          <Tabs />
-        </div>
-      }
-    </div>
-  </section>;
+  return (
+    <section>
+      <div className="max-w-[1170px] px-5 mx-auto">
+        {loading && !error && <Loader />}
+        {error && !loading && <Error />}
+
+        {
+          !loading && !error && (
+            <div className="grid lg:grid-cols-3 gap-[30px] lg:gap-[50px]">
+              <Tabs tab={tab} setTab={setTab} />
+            </div>
+          ) //finished on 4:31
+        }
+      </div>
+    </section>
+  );
 };
 
 export default Dashboard;
--- frontend/src/Dashboard/doctor-account/Tabs.jsx
+++ frontend/src/Dashboard/doctor-account/Tabs.jsx
@@ -1,9 +1,53 @@
+import { BiMenu } from "react-icons/bi";
 
-
-const Tabs = () => {
+const Tabs = ({ tab, setTab }) => {
   return (
-    <div>Tabs</div>
-  )
-}
+    <div>
+      <span className="lg:hidden">
+        <BiMenu className="w-6 h-6 cursor-pointer" />
+      </span>
+      <div className="hidden lg:flex flex-col p-[30px] bg-white shadow-panelShadow items-center h-max rounded-md">
+        <button
+          onClick={() => setTab("overview")}
+          className={`${
+            tab === "overview"
+              ? "bg-indigo-100 text-primaryColor"
+              : "bg-transparent text-headingColor"
+          } w-full btn mt-0 rounded-md`}
+        >
+          Overview
+        </button>
+        <button
+          onClick={() => setTab("appointments")}
+          className={`${
+            tab === "appointments"
+              ? "bg-indigo-100 text-primaryColor"
+              : "bg-transparent text-headingColor"
+          } w-full btn mt-0 rounded-md`}
+        >
+          Appointments
+        </button>
+        <button
+          onClick={() => setTab("settings")}
+          className={`${
+            tab === "settings"
+              ? "bg-indigo-100 text-primaryColor"
+              : "bg-transparent text-headingColor"
+          } w-full btn mt-0 rounded-md`}
+        >
+          Profile
+        </button>
+        <div className="w-full mt-[100px]">
+          <button className="w-full bg-[#181A1E] text-white leading-7 p-3 text-[16px] rounded-md">
+            Logout
+          </button>
+          <button className="w-full bg-red-600 mt-4 text-white leading-7 p-3 text-[16px] rounded-md">
+            Delete account
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Tabs
+export default Tabs;
