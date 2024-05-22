2fdb6e3f2d860bb005ef6694d777a964ed0ae574
Dieudonne-Ibikoraneza <dieudonneibikoraneza13@gmail.com>
2024-05-22 20:43:35 +0200

    Doctor details - halfway there

--- frontend/src/components/Doctors/DoctorCard.jsx
+++ frontend/src/components/Doctors/DoctorCard.jsx
@@ -50,7 +50,7 @@ const DoctorCard = ({ doctor }) => {
         </div>
 
         <Link
-          to="/doctors/1"
+          to={`/${doctor._id}`}
           className="w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] flex items-center justify-center group hover:bg-primaryColor hover:border-none"
         >
           <BsArrowRight className="group-hover:text-white w-6 h-5" />
--- frontend/src/pages/Doctors/DoctorDetails.jsx
+++ frontend/src/pages/Doctors/DoctorDetails.jsx
@@ -1,76 +1,108 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from "react";
-import doctorImg from "../../assets/images/doctor-img02.png";
 import starIcon from "../../assets/images/Star.png";
 import DoctorAbout from "./DoctorAbout";
 import FeedBack from "./FeedBack";
 import SidePanel from "./SidePanel";
+import { BASE_URL } from "../../config";
+import useFetchData from "../../hooks/useFetchData";
+import Loader from "../../components/loader/Loading";
+import Error from "../../components/Error/Error";
+import { useParams } from "react-router-dom";
 
 const DoctorDetails = () => {
   const [tab, setTab] = useState("about");
 
+  const { id } = useParams();
+
+  const {
+    data: doctor,
+    loading,
+    error,
+  } = useFetchData(`${BASE_URL}/doctors/${id}`);
+
+  const {
+    name,
+    qualifications,
+    experiences,
+    timeSlots,
+    reviews,
+    bio,
+    about,
+    averageRating,
+    totalRating,
+    specialization,
+    ticketPrice,
+    photo,
+  } = doctor;
+
   return (
     <section>
       <div className="max-w-[1770px] px-5 mx-auto">
-        <div className="grid md:grid-cols-3 gap-[50px]">
-          <div className="md:col-span-2">
-            <div className="flex items-center gap-5">
-              <figure className="max-w-[200px]  max-h-[200px]">
-                <img src={doctorImg} alt="" className="w-full" />
-              </figure>
+        {loading && <Loader />}
+        {error && <Error />}
 
-              <div>
-                <span className="bg-[#CCF0F3] text-irisBlueColor py-1 px-6 lg:py-2 lg:px-6 text-[12px] leading-4 lg:text-[16px] lg:leading-7 font-semibold rounded">
-                  Surgeon
-                </span>
-                <h3 className="text-headingColor text-[22px] leding-9 mt-3 font-bold">
-                  Muhibur Rahman
-                </h3>
-                <div className="flex items-center gap-[6px]">
-                  <span className="flex items-center gap-[6px] text-[14px] leading-5 lg:text-[16px] lg:leading-7 font-semibold text-headingColor">
-                    <img src={starIcon} alt="" /> 4.8
+        {!loading && !error && (
+          <div className="grid md:grid-cols-3 gap-[50px]">
+            <div className="md:col-span-2">
+              <div className="flex items-center gap-5">
+                <figure className="max-w-[200px]  max-h-[200px]">
+                  <img src={photo} alt="" className="w-full" />
+                </figure>
+
+                <div>
+                  <span className="bg-[#CCF0F3] text-irisBlueColor py-1 px-6 lg:py-2 lg:px-6 text-[12px] leading-4 lg:text-[16px] lg:leading-7 font-semibold rounded">
+                    {specialization}
                   </span>
-                  <span className="text-[14px] leading-5 lg:text-[16px] lg:leading-7 font-[400] text-textColor">
-                    (272)
-                  </span>
+                  <h3 className="text-headingColor text-[22px] leding-9 mt-3 font-bold">
+                    {name}
+                  </h3>
+                  <div className="flex items-center gap-[6px]">
+                    <span className="flex items-center gap-[6px] text-[14px] leading-5 lg:text-[16px] lg:leading-7 font-semibold text-headingColor">
+                      <img src={starIcon} alt="" /> {averageRating}
+                    </span>
+                    <span className="text-[14px] leading-5 lg:text-[16px] lg:leading-7 font-[400] text-textColor">
+                      ({totalRating})
+                    </span>
+                  </div>
+                  <p className="text__para text-[14px] md:text-[15px] leading-6 max-w-[390px]">
+                    {bio}
+                  </p>
                 </div>
-                <p className="text__para text-[14px] md:text-[15px] leading-6 max-w-[390px]">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Natus, dolor.
-                </p>
+              </div>
+
+              <div className="mt-[50px] mb-[20px] border-b border-solid border-[#0066FF43]">
+                <button
+                  onClick={() => setTab("about")}
+                  className={`${
+                    tab === "about" &&
+                    "border-b border-solid border-primaryColor"
+                  } py-2 px-5 mr-5 text-[16px] leading-7 text-headingColor font-semibold `}
+                >
+                  About
+                </button>
+                <button
+                  onClick={() => setTab("feedback")}
+                  className={`${
+                    tab === "feedback" &&
+                    "border-b border-solid border-primaryColor"
+                  } py-2 px-5 mr-5 text-[16px] leading-7 text-headingColor font-semibold `}
+                >
+                  Feedback
+                </button>
+              </div>
+
+              <div className="t-[50px]">
+                {tab === "about" && <DoctorAbout />}
+                {tab === "feedback" && <FeedBack />}
               </div>
             </div>
 
-            <div className="mt-[50px] mb-[20px] border-b border-solid border-[#0066FF43]">
-              <button
-                onClick={() => setTab("about")}
-                className={`${
-                  tab === "about" && "border-b border-solid border-primaryColor"
-                } py-2 px-5 mr-5 text-[16px] leading-7 text-headingColor font-semibold `}
-              >
-                About
-              </button>
-              <button
-                onClick={() => setTab("feedback")}
-                className={`${
-                  tab === "feedback" &&
-                  "border-b border-solid border-primaryColor"
-                } py-2 px-5 mr-5 text-[16px] leading-7 text-headingColor font-semibold `}
-              >
-                Feedback
-              </button>
-            </div>
-
-            <div className="t-[50px]">
-              {tab === "about" && <DoctorAbout />}
-              {tab === "feedback" && <FeedBack />}
+            <div>
+              <SidePanel />
             </div>
           </div>
-
-          <div>
-            <SidePanel />
-          </div>
-        </div>
+        )}
       </div>
     </section>
   );
